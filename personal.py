5c7217b410f0d05c664b90de8201170a38f7c129
Mohamed Vaseem Ismail <mismail@freshworks.com>
2025-02-19 10:02:49 +0530

    personal.py update for admin private note backend

--- personal.py
+++ personal.py
@@ -44,6 +44,42 @@ def allnotes(name):
     else:
         return jsonify({"error":"The data not got yet"})
     
+    
+    
+@app.route("/get-notes-lead-<name>",methods = ["POST"])
+def get_notes(name):
+    data = request.get_json()
+    #Topic =request.get_json()
+    Notes = data.get("Notes")
+    email = data.get("email")
+    # Get current date and time separately
+    date = datetime.now(timezone.utc).date().isoformat()  # "YYYY-MM-DD"
+    time = datetime.now(timezone.utc).time().strftime("%H:%M:%S")  # "HH:MM:SS"
+    
+    if not email or not Notes:
+        return jsonify({"error":"email or Notes are not defined properly, Please Check and try again"}),404
+    notes_lead_admin = notes_admin_db[name]
+    print(notes_lead_admin)
+    insert = notes_lead_admin.insert_one({"name":name,"date":date,"time":time,"email":email,"Notes":Notes})
+    
+    if(insert):
+        return jsonify({"message":"the data has been saved succesfully"}),201
+    else:
+        return jsonify({"error":"the data has not inserted on the database properly"}),404
+    
+    
+@app.route("/get-all-notes-lead/<name>",methods = ["GET"])
+def allnotes(name):
+    #data = request.get_json()
+    notes_lead_admin = notes_lead_db[name]
+    takedata = notes_lead_admin.find({},{"_id":0})
+    print(takedata)
+    takedata_list = list(takedata)
+    if(takedata_list):
+        return jsonify({"data":takedata_list}),200
+    else:
+        return jsonify({"error":"The data not got yet"})
+    
 
     
 
